464f7271b553e0d7a4f54974a7c6fd93532514e2
TKay25 <echelonequipmentimports@gmail.com>
2025-07-17 13:27:52 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -1806,7 +1806,7 @@ def webhook():
                                                         cursor.execute("""
                                                             SELECT id ,empidwa, leavetypewa, startdate, enddate FROM whatsapptempapplication
                                                             WHERE empidwa = %s
-                                                        """, id_user)
+                                                        """, (id_user,))
                                                 
                                                         result = cursor.fetchone()
 
