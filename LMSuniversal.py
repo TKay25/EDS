15360dcdec13143881729c52db5e78428d715fd3
TKay25 <echelonequipmentimports@gmail.com>
2025-07-31 11:26:37 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -10303,7 +10303,7 @@ if connection.status == psycopg2.extensions.STATUS_READY:
             department = request.form.get('department')
             date_applied = request.form.get('dateapplied')
             approver_name = request.form.get('approvername')
-            approver_id = request.form.get('approverid')
+            approver_id = int(request.form.get('approverid'))
             approver_email = request.form.get('approveremailapp')
             approver_whatsapp = request.form.get('approverwhatsappapp')
             leave_days_balance = request.form.get('leavedays-bf')
