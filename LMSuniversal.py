573a7127a3ada3cbebfeb6addd5b7ea5e2e831a4
TKay25 <echelonequipmentimports@gmail.com>
2025-08-19 23:08:45 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -9937,6 +9937,10 @@ def paynow_result():
                 arr = result[3]
                 time = result[4]
                 traveldate = result[11]
+                travel_date_obj = datetime.strptime(traveldate, "%Y-%m-%d")
+
+                # Format as "19 August 2025"
+                formatted_date_travel = travel_date_obj.strftime("%d %B %Y")
                 sender_id = result[1]
                 seats = str(result[12])
                 booking_date = result[10]
@@ -10058,7 +10062,7 @@ def paynow_result():
                         'phone_number': phone55,
                         'id_number':  natidno55,
                         'route': f"{dep} to {arr}",
-                        'travel_date': "date",
+                        'travel_date': formatted_date_travel,
                         "seats": seats,
                         'departure_time': time,
                         'booking_date': 'date',
