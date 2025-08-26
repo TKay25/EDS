c56dda8dea609933038e959620f94b42cb078f69
TKay25 <echelonequipmentimports@gmail.com>
2025-08-26 12:32:49 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -5678,35 +5678,37 @@ def webhook():
                                                         print("bababababababababa")
                                                         print(table_name_apps_pending_approval)
 
-                                                        query = f"SELECT * FROM {table_name_apps_pending_approval} WHERE appid = %s;"
-                                                        print("chiwiwwwwiiiiii1")
-                                                        cursor.execute(query, (int(app_id),))   # ensure it's int
-                                                        print(query)
-
-                                                        print("chiwiwwwwiiiiii2")
-                                                        result = cursor.fetchone()
-                                                        print("chiwiwwwwiiiiii3")
-
-                                                        print(result)
-
-                                                        if result:
-
-                                                            (app_id, employee_number, first_name, surname, department,
-                                                            leave_type, leave_specify, approver_name, approver_id,
-                                                            approver_email, approver_whatsapp, leave_days_balance,
-                                                            date_applied, start_date, end_date, leave_days,
-                                                            leavedaysbalancebf, statuspre) = result
-
-                                                        else:
-
-                                                            print(f"No record found for appid={app_id}")
-
-                                                        #app_id, employee_number, first_name, surname, department, leave_type, leave_specify, approver_name, approver_id, approver_email, approver_whatsapp, leave_days_balance, date_applied, start_date, end_date, leave_days, leavedaysbalancebf, statuspre = result
-                                                        print("chiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
-                                                        print(employee_number)
-                                                        print(approver_name)
 
                                                         try:
+
+                                                            query = f"SELECT * FROM {table_name_apps_pending_approval} WHERE appid = %s;"
+                                                            print("chiwiwwwwiiiiii1")
+                                                            cursor.execute(query, (int(app_id),))   # ensure it's int
+                                                            print(query)
+
+                                                            print("chiwiwwwwiiiiii2")
+                                                            result = cursor.fetchone()
+                                                            print("chiwiwwwwiiiiii3")
+
+                                                            print(result)
+
+                                                            if result:
+
+                                                                (app_id, employee_number, first_name, surname, department,
+                                                                leave_type, leave_specify, approver_name, approver_id,
+                                                                approver_email, approver_whatsapp, leave_days_balance,
+                                                                date_applied, start_date, end_date, leave_days,
+                                                                leavedaysbalancebf, statuspre) = result
+
+                                                            else:
+
+                                                                print(f"No record found for appid={app_id}")
+
+                                                            #app_id, employee_number, first_name, surname, department, leave_type, leave_specify, approver_name, approver_id, approver_email, approver_whatsapp, leave_days_balance, date_applied, start_date, end_date, leave_days, leavedaysbalancebf, statuspre = result
+                                                            print("chiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+                                                            print(employee_number)
+                                                            print(approver_name)
+
                                                             insert_query = f"""
                                                             INSERT INTO {table_name_apps_approved} 
                                                             (appid, id, firstname, surname, department, leavetype, reasonifother, leaveapprovername, leaveapproverid, leaveapproveremail, leaveapproverwhatsapp, currentleavedaysbalance, dateapplied, leavestartdate, leaveenddate, leavedaysappliedfor, leavedaysbalancebf, approvalstatus, statusdate)
@@ -10157,17 +10159,23 @@ def webhook():
                                                             print("bababababababababa")
                                                             print(table_name_apps_pending_approval)
 
-                                                            query = f"SELECT * FROM {table_name_apps_pending_approval} WHERE appid = %s;"
-                                                            cursor.execute(query, (int(app_id),))  
-                                                            print(query)
- 
-                                                            result = cursor.fetchone()
-                                                            app_id, employee_number, first_name, surname, department, leave_type, leave_specify, approver_name, approver_id, approver_email, approver_whatsapp, leave_days_balance, date_applied, start_date, end_date, leave_days, leavedaysbalancebf, statuspre = result
-                                                            print("chiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
-                                                            print(employee_number)
-                                                            print(approver_name)
 
                                                             try:
+
+
+                                                                query = f"SELECT * FROM {table_name_apps_pending_approval} WHERE appid = %s;"
+                                                                cursor.execute(query, (int(app_id),))  
+                                                                print(query)
+    
+                                                                result = cursor.fetchone()
+                                                                print(result)
+                                                                app_id, employee_number, first_name, surname, department, leave_type, leave_specify, approver_name, approver_id, approver_email, approver_whatsapp, leave_days_balance, date_applied, start_date, end_date, leave_days, leavedaysbalancebf, statuspre = result
+                                                                print("chiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+                                                                print(employee_number)
+                                                                print(approver_name)
+
+
+
                                                                 insert_query = f"""
                                                                 INSERT INTO {table_name_apps_approved} 
                                                                 (appid, id, firstname, surname, department, leavetype, reasonifother, leaveapprovername, leaveapproverid, leaveapproveremail, leaveapproverwhatsapp, currentleavedaysbalance, dateapplied, leavestartdate, leaveenddate, leavedaysappliedfor, leavedaysbalancebf, approvalstatus, statusdate)
