410f668ba6117f368604ecccab02ff1689432705
TKay25 <188647232+TKay25@users.noreply.github.com>
2025-10-31 14:23:00 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -17019,8 +17019,9 @@ if connection.status == psycopg2.extensions.STATUS_READY:
                     table_name, rows = results[0]
                     print(f"Table Found: {table_name}")
                     print(rows)
+                    sliced_rows = [row[:15] for row in rows]
 
-                    table_df = pd.DataFrame(rows, columns=['id', 'firstname', 'surname', 'whatsapp', 'address', 'email', 'password', 'department', 'role', 'leaveapprovername', 'leaveapproverid', 'leaveapproveremail','leaveapproverwhatsapp','currentleavedaysbalance', 'monthlyaccumulation'])
+                    table_df = pd.DataFrame(sliced_rows, columns=['id', 'firstname', 'surname', 'whatsapp', 'address', 'email', 'password', 'department', 'role', 'leaveapprovername', 'leaveapproverid', 'leaveapproveremail','leaveapproverwhatsapp','currentleavedaysbalance', 'monthlyaccumulation'])
                     empid = table_df.iat[0,0]
 
                     update_query = f"UPDATE {table_name} SET password = %s WHERE email = %s;"
