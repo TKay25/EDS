316ef759addd07309d81595b5f34d048a52db069
TKay25 <echelonequipmentimports@gmail.com>
2025-11-07 09:22:57 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -19367,4 +19367,5 @@ def explore_lms():
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 55, debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
