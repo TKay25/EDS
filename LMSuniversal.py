61f99d71365a52df5c7a1a8cafff2bc84fb2da1d
TEEKAYZ <99164597+TEEKAYZ@users.noreply.github.com>
2026-01-26 12:38:18 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -16302,7 +16302,7 @@ def run1(table_name, empid):
 
         df_employees_payroll = pd.DataFrame(rowspayroll, columns=["id","Firstname", "Surname","Manager_Supervisor", "Department", "Designation","Date Joined","accnumber","Bank","basicsalary"])
         df_employees_payroll['Action'] = df_employees_payroll.apply(
-            lambda row: f'''<div style="display: flex; gap: 10px;font-size: 12px;"><button class="btn btn-primary3 edit-emp-details-comp-btn-payroll" data-id="{row['id']}" data-firstname="{row['Firstname']}" data-surname="{row['Surname']}" data-manager="{row['Manager_Supervisor']}" data-department="{row['Department']}" data-designation="{row['Designation']}"  data-datejoined="{row['Date Joined']}" data-accnumber="{row['accnumber']}"  data-bank="{row['Bank']}"  data-basicsalary="{row['basicsalary']}">Edit Information</button></div>''', axis=1
+            lambda row: f'''<div style="display: flex; gap: 10px;font-size: 12px;"><button class="btn btn-primary3 edit-emp-details-comp-btn-payroll" data-bs-toggle="modal" data-bs-target="#editEmployeePayrollModal" data-id="{row['id']}" data-firstname="{row['Firstname']}" data-surname="{row['Surname']}" data-manager="{row['Manager_Supervisor']}" data-department="{row['Department']}" data-designation="{row['Designation']}"  data-datejoined="{row['Date Joined']}" data-accnumber="{row['accnumber']}"  data-bank="{row['Bank']}"  data-basicsalary="{row['basicsalary']}">Edit Information</button></div>''', axis=1
         )
 
         df_employees_payroll = df_employees_payroll[["id", "Firstname", "Surname", "Department", "Designation","Bank", "Action"]]
