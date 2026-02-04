4d4c86f71a2fbee8d68f541862c5b72fa0c91512
TEEKAYZ <99164597+TEEKAYZ@users.noreply.github.com>
2026-02-04 10:45:44 +0200

    Update LMSuniversal.py

--- LMSuniversal.py
+++ LMSuniversal.py
@@ -16402,8 +16402,9 @@ def run1(table_name, empid):
         df_employees_payroll['Action'] = df_employees_payroll.apply(
             lambda row: f'''<div style="display: flex; gap: 10px;font-size: 12px;"><button class="btn btn-primary3 edit-emp-details-comp-btn-payroll" data-bs-toggle="modal" data-bs-target="#editEmployeePayrollModal" data-id="{row['id']}" data-firstname="{row['Firstname']}" data-surname="{row['Surname']}" data-manager="{row['Manager_Supervisor']}" data-department="{row['Department']}" data-designation="{row['Designation']}"  data-datejoined="{row['Date Joined']}" data-accnumber="{row['accnumber']}"  data-bank="{row['Bank']}"  data-basicsalary="{row['basicsalary']}">Edit Information</button></div>''', axis=1
         )
+        df_employees_payroll['Employee Name'] = df_employees_payroll['Firstname'] + ' ' + df_employees_payroll['Surname']
 
-        df_employees_payroll = df_employees_payroll[["id", "Firstname", "Surname", "Department", "Designation","Bank", "Action"]]
+        df_employees_payroll = df_employees_payroll[["id", "Employee Name", "Department", "Designation","Bank", "Action"]]
 
         table_employees_payroll_html = df_employees_payroll.to_html(classes="table table-bordered table-theme", table_id="employeespayrollTable", index=False,  escape=False,)
 
